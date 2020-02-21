3617178ba88550f5f4c44e38e5ed74817dda2f63
TammyBee <tammybeetmb@gmail.com>
2020-02-21 22:30:19 +0900

    update.

--- main.js
+++ main.js
@@ -56,8 +56,9 @@ window.onload = function() {
     const button_build_model = document.getElementById('button_build_model');
     const button_generate_sentences = document.getElementById('button_generate_sentences');
 
-    if (localStorage[LOCAL_STORAGE_KEY_SEPARATED_SENTENCES] !== undefined){
+    if (localStorage[LOCAL_STORAGE_KEY_SEPARATED_SENTENCES] != undefined) {
         separated_sentences = JSON.parse(localStorage[LOCAL_STORAGE_KEY_SEPARATED_SENTENCES])["result"];
+        build_current_model_view(separated_sentences);
         markov_chain_model = build_markov_chain_model(separated_sentences, BOS_SYMBOL);
         if (markov_chain_model !== undefined) {
             button_generate_sentences.disabled = false;
@@ -77,6 +78,7 @@ window.onload = function() {
                 return response.json().then(data => {
                     console.log(data)
                     separated_sentences = data["result"];
+                    build_current_model_view(separated_sentences);
                     localStorage[LOCAL_STORAGE_KEY_SEPARATED_SENTENCES] = JSON.stringify(data);
                     markov_chain_model = build_markov_chain_model(separated_sentences, BOS_SYMBOL);
                     if (markov_chain_model !== undefined) {
@@ -84,7 +86,10 @@ window.onload = function() {
                     }
                 });
             }).catch(
-                error => console.log(error)
+                error => {
+                    alert("[ERROR]\nモデルを生成できませんでした。\nしばらく待ってから、やり直してください。\nこのアラートが何度も出る場合は、作者に報告してください。")
+                    console.log(error)
+                }
             ).finally(() => {
                 button_build_model.disabled = false;
                 button_build_model.value = "モデル生成";
@@ -103,6 +108,18 @@ window.onload = function() {
         return new MarkovChain(segments);
     }
 
+    function build_current_model_view(separated_sentences) {
+        const number_of_phrases = document.getElementById("view_model_info_number_of_phrases");
+        number_of_phrases.innerHTML = separated_sentences.length;
+
+        let count = 0;
+        for (let sentence of separated_sentences) {
+            count += sentence.length;
+        }
+        const number_of_segments = document.getElementById("view_model_info_number_of_segments");
+        number_of_segments.innerHTML = count;
+    }
+
     function build_result_view(generated_sentences) {
         let view = `<table>`;
         for (let sentence of generated_sentences) {
@@ -110,7 +127,7 @@ window.onload = function() {
         }
         view += `</table>`;
 
-        const result = document.getElementById("result_generated_sentences");
+        const result = document.getElementById("view_result_generated_sentences");
         result.innerHTML = view;
     }
 
@@ -153,10 +170,10 @@ window.onload = function() {
         let generated_sentences = [];
         for (let i = 0; i < number_of_sentences; i++) {
             let generated_sentence = markov_chain_model.generate_sentence(input_start_word, BOS_SYMBOL, input_contain_string, min_sentence_length, max_sentence_length, max_number_of_trials);
-            if(starts_with_bos_symbol){
+            if (starts_with_bos_symbol) {
                 generated_sentence = generated_sentence.slice(BOS_SYMBOL.length);
             }
-            if(to_halfwidth){
+            if (to_halfwidth) {
                 generated_sentence = generated_sentence.replace(/[Ａ-Ｚａ-ｚ０-９]/g, function(s) {
                     return String.fromCharCode(s.charCodeAt(0) - 65248);
                 });
